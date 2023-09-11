b51a5749d3b3af1193d403c6681f477fcc6f44f5
dcs0spb <s.p.bradley@durham.ac.uk>
2023-09-11 11:11:13 +0100

    Removed topics with fewer than two classifications

--- model/wg6_model.py
+++ model/wg6_model.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import os
+import json
 
 from cmdstanpy import CmdStanModel
 import arviz as az
@@ -25,7 +26,7 @@ print("seed ", seed, "num_samples", num_samples)
 
 cohorts = pd.read_csv(os.path.join("..", "enrolment", "all_cohort_enrolment_rounded.csv"))
 enrolments = pd.read_csv(os.path.join("..", "enrolment", "all_elective_enrolment_rounded.csv"))
-code_set = pd.read_csv(os.path.join("..", "curriculum_content", "ACM_2023_CAH_codes.csv"), dtype="string")
+#code_set = pd.read_csv(os.path.join("..", "curriculum_content", "ACM_2023_CAH_codes.csv"), dtype="string")
 
 le_cohort = preprocessing.LabelEncoder()
 le_elective = preprocessing.LabelEncoder()
@@ -52,9 +53,12 @@ MODULES=list(cohorts['module_count'])
 maxModules = max(MODULES)
 enrolments.sort_values('module_id', inplace=True)
 
-le_topics = preprocessing.LabelEncoder()
-le_topics.fit(code_set['Abbrev'])
 elective_topics = pd.read_csv(os.path.join("..", "curriculum_content", "coded", "stage2_coded_binary.csv"))
+
+# only code the topics that are properly represented in the data set
+code_set = list(set(elective_topics.columns) - set(['institution', 'elective']))
+le_topics = preprocessing.LabelEncoder()
+le_topics.fit(code_set)
 
 # This is a bit naughty because two electives in different institutions 
 # might have the same mcode
@@ -91,11 +95,11 @@ for cohort_id in range(len(cohorts)):
         cohort_module_women[cohort_id,module_num] = module_num_women(cohort_id, module_num)
         cohort_module_men[cohort_id,module_num] = module_num_men(cohort_id, module_num)
         for topic_id in range(len(code_set)):
-            print ("cohort", cohort_id, "module_num", module_num, "topic_id", topic_id)
+#            print ("cohort", cohort_id, "module_num", module_num, "topic_id", topic_id)
             has_topic = module_num_has_topic(cohort_id, module_num, topic_id)
             cohort_module_topic_mapping[cohort_id,module_num,topic_id] = has_topic
             
-print(cohorts) 
+
 
 
 
@@ -111,7 +115,7 @@ buildData={
     "cohort_women": list(cohorts['Women3']),
     "cohort_men": list(cohorts['Men3']),
     "mu_modules_taken": list(cohorts['mu_modules']),
-    "TOPICS": len(code_set.index),
+    "TOPICS": len(code_set),
     "MAXMODULES": max(MODULES),
     "MODULES": MODULES,
     "module_topics": cohort_module_topic_mapping,
@@ -129,4 +133,7 @@ fit_cols = fit_summary.columns.to_flat_index()
 fit_summary.columns = fit_cols
 fit_summary.to_csv('results/cmdstansummary-wg6-seed-' + str(seed) + '-samples-' + str(num_samples) + '.csv')
 
-print(le_topics.inverse_transform(range(len(code_set.index))))
+cohorts.to_csv("cohorts_with_ids.csv", index=False)
+enrolments.to_csv("enrolments_with_ids.csv", index=False)
+with open("topics.json", "w") as fp:
+    json.dump(list(le_topics.inverse_transform(range(len(code_set)))), fp)
